f7446a22a7a3c15e755862b5c146947cd2aec9de
oschre7741 <31477280+oschre7741@users.noreply.github.com>
2017-08-30 09:11:18 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+#this program says hello and greets a person by name
+
+#Olivia Schreiner
+#August 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+
+print("where are you from")
+city = input()
+print("that's cool! " + city + " is nice.")
+
+print("What do you like to do?")
+activity = input()
+print("you like " + activity + "? I like to " + activity + " too!") 
